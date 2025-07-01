3d758afff41af2c7304f940567fb3f6212ff532d
Mi2025Martins <166538997+Mi2025Martins@users.noreply.github.com>
2025-07-01 13:30:11 -0300

    Add files via upload

---
+++ modulo2/aula2_questão2.py
@@ -0,0 +1,16 @@
+#Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+#Armazene o texto "o resultado é"  em uma variável, o valor 10 em outra variável, e o valor 3.5 numa terceira variável.
+#Some os valores da segunda e terceira variável e armazene em outra variável.
+#Imprima todas as variáveis na ordem de criação e imprima também seus tipos.
+
+print("O resultado é")
+print(type("O resultado é"))
+x=10
+print(x)
+print(type(x))
+y=3.5
+print(y)
+print(type(y))
+z=x+y
+print(z)
+print(type(z))
